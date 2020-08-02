c41a3003fddae83a74eda22385be4e0e83b85200
jakedel <42163241+jakedel@users.noreply.github.com>
2020-08-02 15:38:47 -0700

    Add comments to app.py and PlatformIntegrations.py
    Co-authored-by: Hum4n01d <17228477+Hum4n01d@users.noreply.github.com>

--- PlatformIntegrations.py
+++ PlatformIntegrations.py
@@ -2,15 +2,29 @@ from PySide2 import QtCore, QtGui
 
 class PlatformIntegrations(QtCore.QObject):
   def applyMacOsWindowTreatment():
-    # Import macOS-specific libraries
+    '''Apply settings to the macOS window object to make it seamless and force dark mode'''
+
+    # Import macOS-specific libraries inside the function to avoid importing them on all platforms
     import AppKit
     import ctypes
     import objc
 
+    # Get the system window ID of the first window
+    # Other windows like preferences and onboarding should have the standard window style
     window_id = QtGui.QGuiApplication.allWindows()[0].winId()
+
+    # Convert system window ID into Objective-C NSView object and get the NSWindow of the view
     window_view = objc.objc_object(c_void_p=ctypes.c_void_p(window_id))
     window = window_view.window()
-    window.setAppearance_(AppKit.NSAppearance.appearanceNamed_(AppKit.NSAppearanceNameVibrantDark)) # Force dark mode
+    
+    # Force dark mode to avoid the dreaded white line when light mode is active
+    window.setAppearance_(AppKit.NSAppearance.appearanceNamed_(AppKit.NSAppearanceNameVibrantDark)) 
+    
+    # Remove the window titlebar background
     window.setTitlebarAppearsTransparent_(True)
+
+    # Hide title from window titlebar
     window.setTitleVisibility_(AppKit.NSWindowTitleHidden)
-    window.setStyleMask_(window.styleMask() | AppKit.NSFullSizeContentViewWindowMask) # Enable seamless window
+    
+    # Allow window content to overlap the titlebar, creating a seamless window appearance
+    window.setStyleMask_(window.styleMask() | AppKit.NSFullSizeContentViewWindowMask)
--- app.py
+++ app.py
@@ -11,14 +11,12 @@ from ScrobbleHistoryListModel import *
 
 # Get the built application path
 if getattr(sys, 'frozen', False):
-  # If the application is run as a bundle, the PyInstaller bootloader
-  # extends the sys module by a flag frozen=True and sets the app 
-  # path into variable _MEIPASS'.
+  # If the application is run as a bundle, the PyInstaller bootloader extends the sys module by a flag frozen=True and sets the app path into variable _MEIPASS
   application_path = sys._MEIPASS
 else:
   application_path = os.path.dirname(os.path.abspath(__file__))
 
-# Enable Ctrl-C to kill
+# Enable Ctrl-C to kill app
 signal.signal(signal.SIGINT, signal.SIG_DFL)
 
 # Store constants for registering types
@@ -27,7 +25,8 @@ MAJOR_VERSION = 1
 MINOR_VERSION = 0
 
 if __name__ == '__main__':
-  # Register models
+  # Create QML components from Python classes
+  # major_version and minor_version represent major and minor version numbers for when we import it in QML
   QtQml.qmlRegisterType(ScrobbleHistoryViewModel, MODULE_NAME, MAJOR_VERSION, MINOR_VERSION, 'ScrobbleHistoryViewModel')
   QtQml.qmlRegisterType(ScrobbleDetailsViewModel, MODULE_NAME, MAJOR_VERSION, MINOR_VERSION, 'ScrobbleDetailsViewModel')
   QtQml.qmlRegisterType(ScrobbleHistoryListModel, MODULE_NAME, MAJOR_VERSION, MINOR_VERSION, 'ScrobbleHistoryListModel')
@@ -35,11 +34,21 @@ if __name__ == '__main__':
   # Enable retina support
   QtCore.QCoreApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling)
 
-  # Init QtQuick application context
-  os.environ['QSG_RENDER_LOOP'] = 'windows' # Use render loop that supports persistent 6MINOR_VERSIONfps
+  # Use render loop that supports persistent 60fps
+  os.environ['QSG_RENDER_LOOP'] = 'windows'
+
+  # Create system app
   app = QtGui.QGuiApplication(sys.argv)
+
+  # Initialize QML rendering engine
   engine = QtQml.QQmlApplicationEngine(parent=app)
+
+  # Get the main QML file path and load it  
   file = os.path.join(application_path, "main.qml")
   engine.load(QtCore.QUrl.fromLocalFile(file))
+
+  # Apply macOS-specific code which changes the main window to a seamless appearance
   PlatformIntegrations.applyMacOsWindowTreatment()
-  sys.exit(app.exec_())
+  
+  # Use the app's status as an exit code
+  sys.exit(app.exec_()) # exec_ to avoid collision with built in exec function
