ac35b5695f7f5599099cb4fcda694be6792c369c
Hum4n01d <17228477+Hum4n01d@users.noreply.github.com>
2020-09-21 17:43:55 -0700

    Slim down and add annotations to MockPlayerPlugin songs

--- plugins/MockPlayerPlugin.py
+++ plugins/MockPlayerPlugin.py
@@ -1,76 +1,48 @@
-import random
-
 from datatypes.MediaPlayerState import MediaPlayerState
+from plugins.MediaPlayerPlugin import MediaPlayerPlugin
 
-class MockPlayerPlugin():
-  MOCK_TRACKS = [
-  {
-    'track_title': 'Glitter',
-    'artist_name': 'BENEE',
-    'album_title': 'Fire on Marzz - EP',
+class MockPlayerPlugin(MediaPlayerPlugin):
+  MOCK_TRACKS = [{
+    # Test song with no album
+    'track_title': 'Where It\'s At',
+    'artist_name': 'Beck',
     'track_start': 0,
-    'track_finish': 221
+    'track_finish': 100
   }, {
-    'track_title': 'Play',
-    'artist_name': 'Years & Years & Jax Jones',
-    'album_title': 'Palo Santo (Deluxe)',
+    # Test Last.fm corrections API
+    'track_title': 'Waters (feat. Phluze) [Elbor edit]',
+    'artist_name': 'Elbor',
+    'album_title': 'Waters (Elbor edit) [feat. Phluze] - single',
     'track_start': 0,
-    'track_finish': 221
+    'track_finish': 100
   }, {
-    'track_title': 'SouthSide',
-    'artist_name': 'DJ Snake & Eptic',
-    'album_title': 'SouthSide - Single',
+    # Test song with no artist
+    'track_title': 'localtrack.mp3',
     'track_start': 0,
-    'track_finish': 221
+    'track_finish': 100
   }, {
-    'track_title': 'ON',
-    'artist_name': 'BTS',
-    'album_title': 'MAP OF THE SOUL : 7',
-    'track_start': 0,
-    'track_finish': 221
-  }, {
-    'track_title': 'ON',
-    'artist_name': 'BTS',
-    'album_title': 'MAP OF THE SOUL : 7',
-    'track_start': 0,
-    'track_finish': 221
-  }, {
+    # Test song with 3 artists
     'track_title': 'Flames',
     'artist_name': 'R3HAB, ZAYN & Jungleboi',
     'album_title': 'Flames (The EP)',
     'track_start': 0,
-    'track_finish': 221
+    'track_finish': 100
   }, {
-    'track_title': 'On & On (feat. Daniel Levi)',
-    'artist_name': 'Cartoon',
-    'album_title': 'On & On (feat. Daniel Levi) - Single',
-    'track_start': 0,
-    'track_finish': 240
-  }, {
-    'track_title': 'The Way (feat. T.C.)',
-    'artist_name': 'Hum4n01d',
-    'album_title': 'The Way (feat. T.C.) - Single',
-    'track_start': 0,
-    'track_finish': 177
-  }, {
+    # Test artist with diacritical marks in name
     'track_title': 'Grapevine',
     'artist_name': 'Tiësto',
     'album_title': 'Grapevine - Single',
     'track_start': 0,
     'track_finish': 221
   }, {
-    'track_title': 'Afterlife',
-    'artist_name': 'NCT',
-    'album_title': 'Afterlife - Single',
-    'track_start': 0,
-    'track_finish': 221
-  }, {
+    # Test track with censored name
     'track_title': 'C**o',
     'artist_name': 'Jason Derulo, Puri & Jhorrmountain',
     'album_title': 'C**o - Single',
     'track_start': 0,
     'track_finish': 221
   }, {
+    # Test track with super long title
     'track_title': 'Welcome to the Party (feat. Zhavia Ward) [From the "Deadpool 2" Original Motion Picture Soundtrack]',
     'artist_name': 'Diplo, French Montana & Lil Pump',
     'album_title': 'Welcome to the Party (feat. Zhavia Ward) [From the "Deadpool 2" Original Motion Picture Soundtrack] - Single',
@@ -84,6 +56,7 @@ class MockPlayerPlugin():
     self.__next_track_index = -1
     self.player_position = 0
 
+  # Special mock player method
   def get_next_track(self):
     track = self.MOCK_TRACKS[self.__next_track_index % len(self.MOCK_TRACKS)]
     self.__next_track_index += 1
