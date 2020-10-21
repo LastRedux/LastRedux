1e3045adcafba9dfd604fbb59ac27008247f950c
Hum4n01d <17228477+Hum4n01d@users.noreply.github.com>
2020-10-20 22:25:13 -0700

    Automatically refresh Spotify access token when it expires

--- util/spotify_api_helper.py
+++ util/spotify_api_helper.py
@@ -16,29 +16,38 @@ def get_token():
 
   return resp.json()['access_token']
 
-def get_images(track_title, artist_name, album_title=''):
-  stripped_track_title = re.sub(r'[^A-Za-z0-9 ]+', '', track_title)
-  stripped_track_title = stripped_track_title.replace('feat', '')
-  stripped_artist_name = artist_name.replace('&', '')
-  query = f'{stripped_track_title} {stripped_artist_name}'
-
-  resp = requests.get('https://api.spotify.com/v1/search', params={
+def search_tracks(query):
+  return requests.get('https://api.spotify.com/v1/search', params={
     'q': query,
     'type': 'track'
   }, headers={
     'Accept': 'application/json',
     'Authorization': f'Bearer {token}'
-  })
+  }).json()
 
-  track_json = resp.json()
+def get_images(track_title, artist_name, album_title=''):
+  global token 
+
+  stripped_track_title = re.sub(r'[^A-Za-z0-9 ]+', '', track_title)
+  stripped_track_title = stripped_track_title.replace('feat', '')
+  stripped_artist_name = artist_name.replace('&', '')
+  query = f'{stripped_track_title} {stripped_artist_name}'
+
+  search_results = search_tracks(query)
+
+  if 'error' in search_results:
+    if search_results['error']['message'] == 'The access token expired':
+      logger.trace('Refreshed Spotify access token')
+      token = get_token()
+      search_results = search_tracks(query)
 
   track = None
 
-  if not track_json.get('tracks').get('items'):
+  if not search_results.get('tracks').get('items'):
     logger.warning(f'No Spotify search results for: {query} (originally {artist_name} - {track_title})')
     return
 
-  track = track_json['tracks']['items'][0]
+  track = search_results['tracks']['items'][0]
 
   album_image = track['album']['images'][0]['url']
   album_image_small = track['album']['images'][-1]['url']
