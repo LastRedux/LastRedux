f1e92d5e88666bf5249fd83831838b42598b01d4
Hum4n01d <17228477+Hum4n01d@users.noreply.github.com>
2021-01-29 15:19:07 -0800

    Raise exception when calling unimplemented MediaPlayerPlugin functions (thanks @OGKevin!)

--- plugins/MediaPlayerPlugin.py
+++ plugins/MediaPlayerPlugin.py
@@ -7,9 +7,9 @@ from datatypes.MediaPlayerState import MediaPlayerState
 class MediaPlayerPlugin(QtCore.QObject):
   __metaclass__ = ABCMeta
 
-  MEDIA_PLAYER_NAME: str = None
-  MEDIA_PLAYER_ID: str = None
-  IS_SUBMISSION_ENABLED: bool = None
+  MEDIA_PLAYER_NAME: str
+  MEDIA_PLAYER_ID: str
+  IS_SUBMISSION_ENABLED: bool
 
   '''Emitted when the media player is not playing anything'''
   stopped = QtCore.Signal()
@@ -33,16 +33,16 @@ class MediaPlayerPlugin(QtCore.QObject):
   def get_player_position(self) -> float:
     '''Get the media players current playback position in secoonds'''
     
-    pass
+    raise Exception('Function not implemented')
 
   @abstractmethod
   def is_open(self) -> bool:
     '''Return whether or not the media player is open'''
     
-    pass
+    raise Exception('Function not implemented')
 
   @abstractmethod
-  def request_initial_state(self) -> None:
+  def request_initial_state(self) -> MediaPlayerState:
     '''Get a MediaPlayerState object without any system notification from the media player'''
 
-    pass
+    raise Exception('Function not implemented')
